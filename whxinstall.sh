59878039ee3e561769bc84989f98858fa279d3d1
Egor Andreev <egorinvertor@gmail.com>
2025-12-20 12:14:03 +0300

    req.txt added

--- whxinstall.sh
+++ whxinstall.sh
@@ -2,7 +2,7 @@ uv venv --python 3.11
 source .venv/bin/activate
 
 uv pip install -r requirements.txt
-uv pip install --no-deps ./whisperx-3.4.3-py3-none-any.whl
+uv pip install --no-deps whisperx==3.4.3
 
 unset LD_LIBRARY_PATH
 export LD_LIBRARY_PATH="/home/andreeveg/cudnn897/lib:$LD_LIBRARY_PATH"
