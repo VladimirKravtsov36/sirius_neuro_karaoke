4680531d02a33ade85e93cf8693d3c8dff45fb09
Egor Andreev <egorinvertor@gmail.com>
2025-12-22 14:32:01 +0300

    repair #4

--- KaraokeProcessor/KaraokeProcessor.py
+++ KaraokeProcessor/KaraokeProcessor.py
@@ -4,13 +4,14 @@ from .Aligner import *
 from .LyricsProvider import *
 from .ASRService import *
 from .AudioLoader import *
-
+from .LLMTextEditor import *
 
 class KaraokeProcessor:
 
     def __init__(
         self,
         audio_loader : AudioLoader,
+        text_editor: LLMTextEditor, 
         lyrics_provider : LyricsProvider | None,
         asr_service : ASRService,
         aligner : Aligner,
@@ -19,7 +20,7 @@ class KaraokeProcessor:
         self.lyrics_provider = lyrics_provider
         self.asr_service = asr_service
         self.aligner = aligner
-
+        self.text_editor = text_editor
     def process(self) -> Dict:
         audio = self.audio_loader.load()
         asr_result = self.asr_service.transcribe(audio)
